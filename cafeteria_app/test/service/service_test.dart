d3a925d25d13fed263b95bc381e033b81b1673ac
Sedatdynn <dayan.sedat1998@gmail.com>
2023-05-12 14:28:21 +0300

    updated the service test code

--- cafeteria_app/test/service/service_test.dart
+++ cafeteria_app/test/service/service_test.dart
@@ -1,17 +1,18 @@
-import 'package:cafeteria_app/core/init/base/service/base_service.dart';
 import 'package:cafeteria_app/views/home/home_shelf.dart';
 import 'package:cafeteria_app/views/home/service/i_home_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  late IHomeService service;
+  setUp(() {
+    service = HomeService(ProjectNetworkManager.instance.service);
+  });
   test('Fetch foods service test', () async {
-    BaseService service = HomeService(ProjectNetworkManager.instance.service);
     final response = await service.fetchProductItems(1);
     expect(response, isNotEmpty);
   });
 
   test('Datetime day test', () {
-    IHomeService service = HomeService(ProjectNetworkManager.instance.service);
     HomeCubit cubit = HomeCubit(service);
     final response = cubit.checkDay();
     expect(response.toLowerCase().toString() == "friday", true);
